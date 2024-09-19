ee4b65e9b369afc8b3abc9a27e664a4e9c5cf52c
Munich <mmunich@storm.cis.fordham.edu>
2024-09-19 17:06:22 -0400

    added student.h

---
+++ UnsortedArray/Student.h
@@ -0,0 +1,31 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student{
+
+public: 
+
+    Student();
+    Student(string fidn);
+    
+    void setFN(string fn);
+    void setLN(string ln);
+    
+    string getFIDN() const;
+    string getFN() const;
+    string getLN() const;
+    
+    friend ostream& operator<<(ostream& os, const Student& s); //output: "Hanks,Tom: A000"
+
+private:
+
+   string FIDN; //the format should be "A000
+   string FN;
+   string LN;
+
+};
+#endif
