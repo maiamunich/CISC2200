51b59d6ac591bdcc165990664dba346a3657aa9d
Maia Munich <maia.munich@gmail.com>
2024-09-27 15:39:49 -0400

    commiting to Lab1.cpp

--- UnsortedArray/Lab1.cpp
+++ UnsortedArray/Lab1.cpp
@@ -1,28 +1,53 @@
 #include "Student.h"
 #include "UnsortedListbyArray.h"
+#include <iostream> 
+#include <string>
+
+using namespace std; 
 
 int main()
 {
+    UnsortedListByArray studentList; //Create an UnsortedListByArray Object
     string answer;
+    int MAX_ITEM = 6; 
     cout<< "Please follow the prompt to enter students information, maximum number of students is 6. Type 'Y' to begin/continue, type 'N' to stop"<< endl; 
     cin >> answer;
 // create a while loop that checks that we dont go over the maxium abount of student '6' and gives the user the options of yes or no 
 
-    while (answer == "y" || "Y")
+    while ((answer == "y" || answer == "Y")&& studentList.GetLength() < MAX_ITEM)
     {
-
+        sttring fidn, fn, ln;
         //inside of the while loop
         // read off students No. (make sure that this counts for if a student is repeated)
-        cout<<"Student No. "<<  ":"<<endl;//(What data type do i put here )
+        cout<<"Student No. "<< (studentList.GetLength() +1) << ":"<<endl;//(What data type do i put here )
         cout<< "Student FIDN"<< endl;
-        cin>> Student::Student(FIDN); //could this and the other two be setFIDN and SetFN and SetLN
+        cin >> fidn; //could this and the other two be setFIDN and SetFN and SetLN //students fidn
         cout<< "Student First Name:"<< endl; 
-        cin>> Student::setFN(); 
+        cin >> fn;         //students first name 
         cout<< "Student Last Name:"<< endl;
-        cin>> Student::setLN();
-        //students fidn
-        //students first name 
-        //students last name
+        cin >> ln;        //students last name
+
+        ItemType newItem;
+        newItem.Initialize(fidn);
+        newItem.setFN(fn);
+        newItem.setLN(ln);
+    
+        if(studentList.InsertItem(newItem))
+        {
+            "A new student has been inserted." << endl; 
+        }
+        else
+        {
+            cout <<"Student already exisit in the list or list is full."<<endl;
+        }
+        if(studentList.GetLength()<MAX_ITEM)
+        {
+            cout << "Do you want to enter another student? (Y/N): ";
+            cin >> answer; 
+        }
+        {
+            cout<< "Maximum number of students reached. Cannot add more students."<<endl;
+        }
         cout<<"A new item is inserted"<< endl; 
     }
 
