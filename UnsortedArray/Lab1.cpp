97d6195206831edd597a0a33c6ae776e85a0b496
Maia Munich <maia.munich@gmail.com>
2024-09-26 23:06:15 -0400

    commit lab1.cpp

--- UnsortedArray/Lab1.cpp
+++ UnsortedArray/Lab1.cpp
@@ -2,5 +2,25 @@
 
 int main()
 {
+   cout<< "Please follow the prompt to enter students information, maximum number of
+students is 6. Type "Y" to begin/continue, type "N" to stop"<< endl; 
+// create a while loop that checks that we dont go over the maxium abount of student '6' and gives the user the options of yes or no 
+
+    //inside of the while loop
+    // read off students No. (make sure that this counts for if a student is repeated)
+    //students fidn
+    //students first name 
+    //students last name
+
+    // let user know that a new item has been inserted
+    //let user know if item was not inserted since it was a duplicate
+//out side while loop 
+// as user if they want to contine
+//when while loop finished let them know they have reached the max capacity of students they could enter
+// output all students using print function 
+
+// as for FIDN for the key value to split the tudent list
+//show the lists split (listone and listtwo)
+//show original list 
     return 0;
 }
