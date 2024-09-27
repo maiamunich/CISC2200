91fb1729bd49b08a307f88a498e57afd551832f4
Maia Munich <maia.munich@gmail.com>
2024-09-27 10:07:15 -0400

    addeed to Lab1.cpp

--- UnsortedArray/Lab1.cpp
+++ UnsortedArray/Lab1.cpp
@@ -2,15 +2,30 @@
 
 int main()
 {
+    string answer;
+    string FIDN, FN, LN;
    cout<< "Please follow the prompt to enter students information, maximum number of
 students is 6. Type "Y" to begin/continue, type "N" to stop"<< endl; 
+    cin >> answer;
 // create a while loop that checks that we dont go over the maxium abount of student '6' and gives the user the options of yes or no 
+
+    while (answer == "y" || "Y")
+    {
 
     //inside of the while loop
     // read off students No. (make sure that this counts for if a student is repeated)
+    cout<<"Student No. "<< //(What data type do i put here )<< ":"<<endl;
+    cout<< "Student FIDN"<< endl;
+    cin>> FIDN;
+    cout<< "Student First Name:"<< endl; 
+    cin>> FN; 
+    cout<< "Student Last Name:"<< endl;
+    cin>>LN;
     //students fidn
     //students first name 
     //students last name
+    cout<<"A new item is inserted"<< endl; 
+    }
 
     // let user know that a new item has been inserted
     //let user know if item was not inserted since it was a duplicate
