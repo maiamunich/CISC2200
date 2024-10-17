71aff54b59fea773f12a5b6b8ab1998ba597be7c
Maia Munich <maia.munich@gmail.com>
2024-10-16 23:12:15 -0400

    dfghj

--- SortedList/Lab2.cpp
+++ SortedList/Lab2.cpp
@@ -15,25 +15,25 @@ int main()
     cin >> length1;
     for(int a = 0; a <length1; a++)
     {
-        cout<<"Insert the number for position "<< a << ":" << endl;
+        cout<<"Insert the number for position "<< a +1<< ":" << endl;
         cin>> space1;
         item1.Initialize(space1);
         listOne.InsertItem(item1);
     }
     listOne.Print();
 
-    cout<< "Please enter how long is the lsit two is: ";
+    cout<< "Please enter how long is the list two is: ";
     cin>> length2;
     for(int b = 0; b< length2; b++)
     {
-        cout<< "Insert the number for position"<< b<< ":"<< endl;
+        cout<< "Insert the number for position"<< b +1<< ":"<< endl;
         cin>> space2;
         item2.Initialize(space2);
         listTwo.InsertItem(item2);
     }
     listTwo.Print(); 
 
-    
+
 
     //sorting the lists one 
   //  for(int i = 0; i < listOne.GetLength(); i++)
@@ -62,7 +62,7 @@ int main()
         }
     }
 */
-
+/*
     cout<< "The outcome of your inserted number comes out too these two list: "<< endl;
     listOne.Print();
 
@@ -70,8 +70,8 @@ int main()
 
     listTwo.Print();
 
+*/
     MergedList.MergeList(listOne, listTwo);
-
     cout<< "After merging both list together we get a sorted list: "<< endl;
     
     MergedList.Print();
