f519b257e1fdf28872bbdf923bb02ddf8961d602
Maia Munich <maia.munich@gmail.com>
2024-10-25 16:54:21 -0400

    the new code

---
+++ Stack/Lab3.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<stack> 
+
+bool isBalanced(cosnt string& case)
+{
+    stack<char> s; 
+
+    for (char ch : case)
+    {
+        //push onto stack
+        if(ch == '()' || ch == '{' || ch == '[')
+        {
+            s.push(ch);
+        }
+         //checking stack
+        else if (ch == ')' || ch == '}' || ch == ']')
+        {
+             // if stack empty means we have an unmatched pair of closing brackets 
+       
+            if(s.empty()) return false;
+
+            char top = s.top();
+
+            //this condition check if the top and bottom match each other
+            if ((ch == ')' && top == '(') || (ch == '}' && top == '[') || (ch == ']' && top == '['))
+            {
+             //pop the matching opening bracket
+                s.pop();
+            }
+            else 
+            {
+                return false; 
+            //have mismatching brackets
+            }
+        }
+    }
+    return s.empty();
+}
+
+int main ()
+{
+    string case;
+    cout << "Enter an expression to check if parentheses are balanced: ";
+    getline(cin, case);
+
+    if( isBalanced(case))
+    {
+        cout<< "The parenthese are balance."<<endl;
+    }
+    else 
+    {
+        cout<< "The parentheses are not balanced."<<endl;
+    }
+    return 0;
+}
+   
