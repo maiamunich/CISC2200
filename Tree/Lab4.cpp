4f8ecebba436dda1993eab93e6774463b290c8b3
Mario E. Munich <mariomu@ieee.org>
2024-11-20 18:27:29 -0800

    Added all the steps in Lab4

--- Tree/Lab4.cpp
+++ Tree/Lab4.cpp
@@ -16,9 +16,29 @@ int main()
         tree.InsertItem(item);
     }
     
-    // Print the tree 
+    // Print the tree (in-order)
     tree.Print();
 
+    // Count the leaves in the tree
+    cout << "The tree has" << tree.LeafCount() << "elements" << endl;
+
+    // Test ancestors
+    cout << "Test Ancestors for: C" << endl;
+    item.Initialize('C');
+    tree.Ancestors(item);
+
+    // Test descendants
+    cout << "Test Descendants for: N" << endl;
+    item.Initialize('N');
+    tree.Descendants(item);
+
+    // Test Swap
+    Tree newTree;
+    tree.Swap(newTree);
+    newTree.Print();
+
+    // Test LeafCount()
+    cout << "There are" << tree.LeafCount() << "leaf nodes" << endl;
 
     return 0;
 }
